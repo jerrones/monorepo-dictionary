5bf91cfa120c6bbc2bb7fa5d50b687d075ff96cb
Jerry Jones <jerryjfernandes@icloud.com>
2026-06-21 17:10:24 -0300

    front@chore: change global configs

--- apps/front/next.config.ts
+++ apps/front/next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: process.platform === "win32" ? undefined : "standalone",
+};
 
 export default nextConfig;
